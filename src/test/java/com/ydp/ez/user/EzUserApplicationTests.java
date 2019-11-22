4a68d8b8e9993a6d067061418531d9db8ef66927
叶道平 <daoping.ye@56qq.com>
2019-11-22 09:04:34 +0800

    集成druid

--- src/test/java/com/ydp/ez/user/EzUserApplicationTests.java
+++ src/test/java/com/ydp/ez/user/EzUserApplicationTests.java
@@ -3,11 +3,27 @@ package com.ydp.ez.user;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class EzUserApplicationTests {
+import javax.annotation.Resource;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
-	@Test
-	void contextLoads() {
-	}
+@SpringBootTest()
+class EzUserApplicationTests {
+    @Resource
+    DataSource dataSource;
+    @Test
+    public void contextLoads() throws SQLException {
+        Connection connection = dataSource.getConnection();
+        PreparedStatement prepareStatement = connection
+                .prepareStatement("select * from user where id='1'");
+        ResultSet resultSet = prepareStatement.executeQuery();
+        while (resultSet.next()) {
+            String cityName = resultSet.getString("user_name");
+            System.out.println(cityName);
+        }
+    }
 
 }
